2afa9907865c5375f5a7886e594f57bdfafb3e7e
jihadsoyon <jihadsoyon@gmail.com>
2026-07-25 23:59:46 +0600

    Dependency install & Navbar, bannerm & footer section designed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between items-center bg-white p-3'>
+            <ul className='flex gap-3'>
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/destinations'}>Destinations</Link></li>
+                <li><Link href={'/my-bookings'}>My Bookings</Link></li>
+            </ul>
+
+            <div>
+                <Image
+                    src={'/assets/Wanderlast.png'}
+                    height={150}
+                    width={150}
+                    alt='logo'
+
+                />
+
+            </div>
+
+            <ul className='flex gap-3'>
+                <li><Link href={'/profile'}>Profile</Link></li>
+                <li><Link href={'/signin'}>Sign in</Link></li>
+                <li><Link href={'/signup'}>Sign up</Link></li>
+            </ul>
+
+        </nav>
+    );
+};
+
+export default Navbar;
